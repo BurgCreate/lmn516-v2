69d230a3df1eb02e91d1a23aaa70606c4827977d
BurgCreate <burgcreate000@gmail.com>
2026-07-26 14:26:09 +0800

    Fix WordPress excerpt ellipsis

--- lib/wordpress.ts
+++ lib/wordpress.ts
@@ -44,6 +44,8 @@ const fallbackPosts: Post[] = [
 function cleanHtml(input: string) {
   return input
     .replace(/<[^>]+>/g, "")
+    .replace(/\[&hellip;\]/g, "")
+    .replace(/&hellip;/g, "…")
     .replace(/&nbsp;/g, " ")
     .replace(/&#8217;/g, "’")
     .replace(/&#8220;|&#8221;/g, "“")
